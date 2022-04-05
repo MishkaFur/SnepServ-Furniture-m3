7b7f2e04fe76eefd2b339937495bbfae5122d0cb
MishkaFur <61889138+MishkaFur@users.noreply.github.com>
2022-04-05 19:33:35 +0100

    Initial Commit

---
+++ SnepServ-Furniture-m3/Furniture-m3Hard/AshlarLargeGraniteFountain.cs
@@ -0,0 +1,15 @@
+﻿namespace Eco.Mods.TechTree
+{
+
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Property;
+
+    [RequireComponent(typeof(RoomRequirementsComponent))]
+    [RequireRoomContainment]
+    [RequireRoomVolume(72)] 
+    public partial class AshlarLargeGraniteFountainObject
+    {
+
+    }
+}
---
+++ SnepServ-Furniture-m3/Furniture-m3Normal/PlasticChairGreen.cs
@@ -0,0 +1,15 @@
+﻿namespace Eco.Mods.TechTree
+{
+
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Property;
+
+    [RequireComponent(typeof(RoomRequirementsComponent))]
+    [RequireRoomContainment]
+    [RequireRoomVolume(4)] 
+    public partial class PlasticChairGreenObject
+    {
+
+    }
+}
